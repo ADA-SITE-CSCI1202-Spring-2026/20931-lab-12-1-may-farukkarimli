1bd74d0e1bd2b087f7d1cd72af6c790e48593f2d
farukkarimli <fkarimli23291@ada.edu.az>
2026-05-01 09:20:36 +0400

    Added the Grade Filter Code

---
+++ GradeFilter.java
@@ -0,0 +1,46 @@
+import java.util.HashMap;
+
+@FunctionalInterface
+interface GradeRule {
+    boolean test(int grade);
+}
+
+public class GradeFilter {
+    public static void main(String[] args) {
+        HashMap<String, Integer> grades = new HashMap<>();
+
+        grades.put("Ali", 85);
+        grades.put("Leyla", 92);
+        grades.put("Murad", 55);
+        grades.put("Nigar", 70);
+        grades.put("Kamran", 40);
+
+        // ===== TODO 1 =====
+        // Passed rule (>= 50)
+        GradeRule passedRule = grade -> grade >= 50;
+
+        // ===== TODO 2 =====
+        // Excellent rule (>= 85)
+        GradeRule excellentRule = grade -> grade >= 85;
+
+        System.out.println("Passed students:");
+
+        // ===== TODO 3 =====
+        // Print passed students
+        grades.forEach((name, grade) -> {
+            if (passedRule.test(grade)) {
+                System.out.println(name + " -> " + grade);
+            }
+        });
+
+        System.out.println("\nExcellent students:");
+
+        // ===== TODO 4 =====
+        // Print excellent students
+        grades.forEach((name, grade) -> {
+            if (excellentRule.test(grade)) {
+                System.out.println(name + " -> " + grade);
+            }
+        });
+    }
+}
