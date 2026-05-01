7128d0a71cbf099f8f0bf1d54aa3aa5649bc632b
farukkarimli <fkarimli23291@ada.edu.az>
2026-05-01 09:32:16 +0400

    Fixed the bug

---
+++ ThreadLambdaTask.java
@@ -0,0 +1,18 @@
+public class ThreadLambdaTask {
+    public static void main(String[] args) {
+
+        // ===== TODO 1 =====
+        // Create a thread using lambda
+        Thread t = new Thread(() -> {
+            for (int i = 1; i <= 5; i++) {
+                System.out.println(i);
+            }
+        });
+
+        // ===== TODO 2 =====
+        // Start the thread
+        t.start();
+
+        System.out.println("Main method finished.");
+    }
+}
